e49e4f3dba8787c5c781502d36f1e481f56a9793
Henry Lee <hamn07@gmail.com>
2015-08-19 07:50:44 +0800

    Tail

---
+++ test/Tail-Log-Receive-Data.php
@@ -0,0 +1,6 @@
+<?
+  $s_device = isset($_GET['device'])?$_GET['device']:"no device";
+  $s_data   = isset($_GET['data'])?$_GET['data']:"no data";
+  date_default_timezone_set('Asia/Taipei');
+  $s_datetime = date('m/d/Y h:i:s a', time());
+  file_put_contents ("Tail-Log.log","$s_datetime [$s_device] $s_data\n",FILE_APPEND);
