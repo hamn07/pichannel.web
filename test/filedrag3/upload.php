4cd8509c6e1b33b7a6a105e3458dbefad65601ab
Henry Lee <henry@tomin.tw>
2016-01-21 15:36:36 +0800

    by eclipse

--- test/filedrag3/upload.php
+++ test/filedrag3/upload.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 /*
 Server-side PHP file upload code for HTML5 File Drag & Drop demonstration
@@ -33,42 +33,4 @@ else {
 			echo "<p>File $fn uploaded.</p>";
 		}
 	}
-
-=======
-<?php
-/*
-Server-side PHP file upload code for HTML5 File Drag & Drop demonstration
-Featured on SitePoint.com
-Developed by Craig Buckler (@craigbuckler) of OptimalWorks.net
-*/
-$fn = (isset($_SERVER['HTTP_X_FILENAME']) ? $_SERVER['HTTP_X_FILENAME'] : false);
-
-if ($fn) {
-
-	// AJAX call
-	file_put_contents(
-		'uploads/' . $fn,
-		file_get_contents('php://input')
-	);
-	echo "$fn uploaded";
-	exit();
-
-}
-else {
-
-	// form submit
-	$files = $_FILES['fileselect'];
-
-	foreach ($files['error'] as $id => $err) {
-		if ($err == UPLOAD_ERR_OK) {
-			$fn = $files['name'][$id];
-			move_uploaded_file(
-				$files['tmp_name'][$id],
-				'uploads/' . $fn
-			);
-			echo "<p>File $fn uploaded.</p>";
-		}
-	}
-
->>>>>>> 55667ed83ef5b952548a25be3ab5b0b080404810
 }
